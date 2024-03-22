5616c1ac2c6735c057fdc314ec854a6071f59ec6
Luana Satt <alan.salvaterra@gmail.com>
2024-03-22 06:41:45 -0300

    Funcionalidade seleção padrão - Falta botão favoritar

--- src/app/components/standard-selection/standard-selection.component.ts
+++ src/app/components/standard-selection/standard-selection.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+export interface ImageData {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+  imageSize: 'pequeno' | 'grande';
+}
 
 @Component({
   selector: 'app-standard-selection',
@@ -7,14 +17,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./standard-selection.component.css']
 })
 export class StandardSelectionComponent implements OnInit {
+  imageId: number | null = null; // Alterado para permitir valor nulo
+  imageSize: 'pequeno' | 'grande' = 'pequeno'; // Inicializa com 'pequeno' como padrão
+  selectedImage: ImageData | null = null;
 
-  constructor(private router: Router,) { }
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
 
+  getImageById() {
+    if (this.imageId !== null && this.imageId >= 1 && this.imageId <= 5000) { // Verifica se imageId não é nulo
+      this.http.get<ImageData>(`https://jsonplaceholder.typicode.com/photos/${this.imageId}`)
+        .subscribe(
+          (imageData: ImageData) => {
+            this.selectedImage = { ...imageData, imageSize: this.imageSize }; // Define o tamanho selecionado
+          },
+          (error) => {
+            console.error('Erro ao buscar imagem:', error);
+            this.selectedImage = null;
+          }
+        );
+    } else {
+      this.selectedImage = null;
+      console.error('ID fora do intervalo (1 a 5000)');
+    }
+  }
+
+  onSizeChange(event: any) {
+    this.imageSize = event.target.value;
+    if (this.selectedImage) {
+      this.selectedImage.imageSize = this.imageSize; // Atualiza o tamanho selecionado se houver uma imagem selecionada
+    }
+  }
+
+  getImageUrl(): string {
+    if (this.selectedImage) {
+      return this.selectedImage.imageSize === 'grande' ? this.selectedImage.url : this.selectedImage.thumbnailUrl ?? '';
+    }
+    return ''; // Retorna uma string vazia se selectedImage for nulo
+  }
+
+
   navigateToHome() {
     this.router.navigate(['/home']);
   }
-
 }
